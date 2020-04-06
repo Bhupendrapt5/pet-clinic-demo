d8b94141af71208b0d095b2ed89c12e4614ba2fe
bhupendra <bhupendrapt95@gmail.com>
2020-04-06 19:27:55 +0530

    Adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package io.bhupendra.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package io.bhupendra.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
