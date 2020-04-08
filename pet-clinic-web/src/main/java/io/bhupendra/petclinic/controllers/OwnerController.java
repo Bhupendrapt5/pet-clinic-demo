4385396b9b01c25b5ed17ee384ca974b97ab4ad6
bhupendra <bhupendrapt95@gmail.com>
2020-04-09 01:49:52 +0530

    Some minor optimization

--- pet-clinic-web/src/main/java/io/bhupendra/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/io/bhupendra/petclinic/controllers/OwnerController.java
@@ -3,11 +3,12 @@ package io.bhupendra.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwner() {
         return "owners/index";
     }
