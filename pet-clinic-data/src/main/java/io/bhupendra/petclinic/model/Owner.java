fc2a15bca826f397083eb0917247bce47af0abc5
bhupendra <bhupendrapt95@gmail.com>
2020-04-12 20:47:38 +0530

    added contact properties. Closes #28

--- pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Owner.java
@@ -3,7 +3,36 @@ package io.bhupendra.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
-    Set<Pet> pets;
+
+    private String address;
+    private String city;
+    private String mobile;
+
+    private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 
     public Set<Pet> getPets() {
         return pets;
