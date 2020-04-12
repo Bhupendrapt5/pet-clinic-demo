028dd941d52c9a20ed99a5e5221f4cd5131d854f
bhupendra <bhupendrapt95@gmail.com>
2020-04-12 20:43:45 +0530

    Added Specialty entity , associated to Vet. Closes #27

--- pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package io.bhupendra.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Specialty> specialties;
 }
