3f83112f3294f3f5749644f8ef9fe7c0cd793cf9
bhupendra <bhupendrapt95@gmail.com>
2020-04-13 16:53:27 +0530

    Added Specialities map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package io.bhupendra.petclinic.services;
+
+import io.bhupendra.petclinic.model.Specialty;
+
+public interface SpecialitiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/io/bhupendra/petclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package io.bhupendra.petclinic.services.map;
+
+import io.bhupendra.petclinic.model.Specialty;
+import io.bhupendra.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Specialty, Long> implements SpecialitiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
