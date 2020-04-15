5a8ad415f9b154facea26e469ead6cd6823fd990
bhupendra <bhupendrapt95@gmail.com>
2020-04-15 20:20:21 +0530

    Updated Owners with JPA entity mapping. Closes #36

--- pet-clinic-web/src/main/java/io/bhupendra/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/io/bhupendra/petclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets","/vets.html"})
+@RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
