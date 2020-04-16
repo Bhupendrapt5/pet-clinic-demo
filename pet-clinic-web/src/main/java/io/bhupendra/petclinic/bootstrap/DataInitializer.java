30d158bcc8d95bf4145fa800bf3beed4a42f16eb
bhupendra <bhupendrapt95@gmail.com>
2020-04-16 20:06:43 +0530

    Implemented JPA Visit service, added visit on Startup data. Closes #47

--- pet-clinic-web/src/main/java/io/bhupendra/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/io/bhupendra/petclinic/bootstrap/DataInitializer.java
@@ -1,10 +1,7 @@
 package io.bhupendra.petclinic.bootstrap;
 
 import io.bhupendra.petclinic.model.*;
-import io.bhupendra.petclinic.services.OwnerService;
-import io.bhupendra.petclinic.services.PetTypeService;
-import io.bhupendra.petclinic.services.SpecialityService;
-import io.bhupendra.petclinic.services.VetService;
+import io.bhupendra.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,50 +14,60 @@ public class DataInitializer implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                           SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
 
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         int count = petTypeService.findAll().size();
-        if(count ==0){
+        if (count == 0) {
             loadData();
         }
     }
 
     private void loadData() {
+        //Adding Pet
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
 
+        //Adding Pet
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        //Adding Pet
         PetType turtle = new PetType();
         turtle.setName("Turtle");
         PetType savedTurtlePetType = petTypeService.save(turtle);
 
         System.out.println("Loaded PetTypes");
 
+        //Adding Speciality
         Speciality radiology = new Speciality();
         radiology.setDescription("Radiology");
         Speciality saveRadiology = specialityService.save(radiology);
 
+        //Adding Speciality
         Speciality surgery = new Speciality();
         surgery.setDescription("Surgery");
         Speciality saveSurgery = specialityService.save(surgery);
 
+        //Adding Speciality
         Speciality dentistry = new Speciality();
         dentistry.setDescription("Dentistry");
         Speciality saveDentistry = specialityService.save(dentistry);
 
+        //Adding Owner
         Owner owner1 = new Owner();
         owner1.setFirstName("Abhi");
         owner1.setLastName("Gupta");
@@ -68,6 +75,7 @@ public class DataInitializer implements CommandLineRunner {
         owner1.setCity("Mumbai");
         owner1.setMobile("963852741");
 
+        //Adding Owner Pet
         Pet abiPet = new Pet();
         abiPet.setPetType(savedDogPetType);
         abiPet.setOwner(owner1);
@@ -78,6 +86,7 @@ public class DataInitializer implements CommandLineRunner {
 
         ownerService.save(owner1);
 
+        //Adding Owner
         Owner owner2 = new Owner();
         owner2.setFirstName("Bruce");
         owner2.setLastName("Wayne");
@@ -85,6 +94,7 @@ public class DataInitializer implements CommandLineRunner {
         owner2.setCity("Gotham");
         owner2.setMobile("789528596");
 
+        //Adding Owner Pet
         Pet bruPet = new Pet();
         bruPet.setPetType(savedCatPetType);
         bruPet.setOwner(owner2);
@@ -96,6 +106,16 @@ public class DataInitializer implements CommandLineRunner {
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners....");
+
+        //Adding Visit
+        Visit catVisit = new Visit();
+        catVisit.setPet(bruPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
+
+        System.out.println("Loaded Visit....");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
